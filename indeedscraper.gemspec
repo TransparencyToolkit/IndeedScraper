db21918f4c4753c390b821bfa270ac9eb3647e65
M. C. McGrath <shidash@shidash.com>
2014-06-01 13:01:51 -0700

    Added job scraper

---
+++ indeedscraper.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'indeedscraper'
+  s.version     = '0.0.1'
+  s.date        = '2014-06-01'
+  s.summary     = 'Get resumes and job listings from indeed.'
+  s.description = 'Get resumes and job listings from indeed based on search terms and locations.'
+  s.authors     = ['M. C. McGrath']
+  s.email       = 'shidash@shidash.com'
+  s.files       = ["lib/indeedscraper.rb"]
+  s.homepage    =
+    'https://github.com/TransparencyToolkit/IndeedScraper'
+  s.license       = 'GPL'
+end
--- lib/indeedscraper.rb
+++ lib/indeedscraper.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'json'
 require 'nokogiri'
 require 'date'
+require 'open_uri_redirections'
 
 class IndeedScraper
   def initialize(searchterm, location)
@@ -162,13 +163,60 @@ class IndeedScraper
     end
   end
 
+  # Search for jobs
   def searchJobs
-    # Save company/job name
-    # Save location
-    # Save url
-    # Download url
-    # Get page text
-    # Add each to the output
+    @searchterm.gsub!(" ", "+")
+    if @location != nil
+      @location.gsub!(", ", "%2C+")
+      @location.gsub!(" ", "+")
+      url = "http://www.indeed.com/jobs?q=" + @searchterm + "&l=" + @location
+    else
+      url = "http://www.indeed.com/jobs?q=" + @searchterm + "&l="
+    end
+    html = Nokogiri::HTML(open(url))
+
+    # Handle multiple pages
+    numresults = html.css("div#searchCount").text.split(" of ")
+    fresult = numresults[1].to_i/10.0
+    if fresult != numresults[1].to_i/10
+      count = fresult +1
+    else
+      count = numresults[1].to_i/10
+    end
+
+    # Loop through pages and get results
+    i = 1
+    while i <= count
+      # Parse each listing
+      html.css("div.row").each do |r|
+        jobhash = Hash.new
+        jobhash[:position] = r.css("h2.jobtitle").text.strip.lstrip
+        jobhash[:company] = r.css("span.company").text.strip.lstrip
+        jobhash[:location] = r.css('span[itemprop="jobLocation"]').text.strip.lstrip
+        if r.css("h2.jobtitle").css("a")[0]
+          jobhash[:url] = "http://indeed.com" + r.css("h2.jobtitle").css("a")[0]["href"]
+          begin
+            jobhash[:text] = Nokogiri::HTML(open(jobhash[:url])).text
+          rescue
+            begin
+              jobhash[:text] = Nokogiri::HTML(open(jobhash[:url], :allow_redirections => :all)).text
+            rescue
+            end
+          end
+        end
+        @output.push(jobhash)
+      end
+
+      # Get next page
+      i += 1
+      nextstart = (i-1)*10
+      if @location != nil
+        url = "http://www.indeed.com/jobs?q=" + @searchterm + "&l=" + @location + "&start=" + nextstart.to_s
+      else
+        url = "http://www.indeed.com/jobs?q=" + @searchterm + "&start=" + nextstart.to_s
+      end
+      html = Nokogiri::HTML(open(url))
+    end
   end
 
   # Generates JSON output
@@ -176,4 +224,3 @@ class IndeedScraper
     JSON.pretty_generate(@output)
   end
 end
-
