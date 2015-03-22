0a839ce0ec20970f14ed093884112d8cdc86aa71
GH <shidash@shidash.com>
2015-03-23 00:04:59 +0100

    Added education section parsing

--- version2/indeedscraper.go
+++ version2/indeedscraper.go
@@ -7,11 +7,14 @@ import (
   "io/ioutil"
 	"strings"
 	"strconv"
-	//"encoding/json"
+  //"reflect"
+	"encoding/json"
   "github.com/moovweb/gokogiri"
+  "github.com/moovweb/gokogiri/xml"
 )
 
 // Parse and save profiles
+// Refactor add all
 // Output in JSON (clean up formatting)
 // Add timestamp
 
@@ -19,6 +22,8 @@ import (
 // Add concurrency (in parsing and pages?)
 
 // Add same for job listing
+
+var overall []map[string]string
 
 func main(){
   // Generate search URL
@@ -49,7 +54,9 @@ func main(){
     //getResults(url + "&start="+strconv.Itoa(i*50))
   }
   
-  parseProfile("http://www.indeed.com/r/Adonis-Peralta/805b36f3e9d6f2fa")
+  parseProfile("http://www.indeed.com/r/Marilyn-Schlitz/2c0daadc5fb24f45")
+  out, _ := json.Marshal(overall)
+  fmt.Println(string(out))
 }
 
 // Gets the results for a single page
@@ -81,6 +88,15 @@ func getPageCount(firstpage []uint8) int {
 	return numpages
 }
 
+// Return cleaned up value if input isn't empty
+func checkVal(input []xml.Node) string {
+  if(len(input) != 0){
+    return input[0].InnerHtml()
+  } else {
+    return ""
+  }
+}
+
 // Parses and saves data in profile
 func parseProfile(url string) {
   body := getPage(url)
@@ -90,25 +106,25 @@ func parseProfile(url string) {
   personvals := make(map[string]string)
 	
   name, _ := doc.Search("//h1[@itemprop='name']")
-  personvals["name"] = name[0].InnerHtml()
+  personvals["name"] = checkVal(name)
 	
   personvals["url"] = url
   personvals["fulltext"] = string(body)
 	
   location, _ := doc.Search("//p[@id='headline_location']")
-  personvals["location"] = location[0].InnerHtml()
+  personvals["location"] = checkVal(location)
 	
   current_title, _ := doc.Search("//h2[@id='headline']")
-  personvals["current_title"] = current_title[0].InnerHtml()
+  personvals["current_title"] = checkVal(current_title)
 	
   skills, _ := doc.Search("//div[@id='skills-section']//p")
-  personvals["skills"] = skills[0].InnerHtml()
+  personvals["skills"] = checkVal(skills)
 
   summary, _ := doc.Search("//p[@id='res_summary']")
-  personvals["summary"] = summary[0].InnerHtml()
+  personvals["summary"] = checkVal(summary)
 	
   additional_info, _ := doc.Search("//div[@id='additionalinfo-section']//p")
-  personvals["additional_info"] = additional_info[0].InnerHtml()
+  personvals["additional_info"] = checkVal(additional_info)
 
 	//out, _ := json.Marshal(personvals)
 	//fmt.Println(string(out))
@@ -117,8 +133,8 @@ func parseProfile(url string) {
 	//data, _ := jobs[0].Search("//div[@class='data_display']")
 	//for _, job := range(jobs) {       
   //job_title, _ := job.Search("//p[@class='work_title title']")
-  company, _ := doc.Search("//div[@class='work_company']")
-  fmt.Println(company)
+ // company, _ := doc.Search("//div[@class='work_company']")
+  //fmt.Println(company)
 	//for i, _ := range(job_title){
 	//reflect.TypeOf(data)
 	//fmt.Println(data)
@@ -131,8 +147,46 @@ func parseProfile(url string) {
 	// Job Description
 
   // Education info
+  degrees, _ := doc.Search("//div[@itemtype='http://schema.org/EducationalOrganization']")
+  for _, degree := range(degrees){
+    degreevals := make(map[string]string)
+    
+    school, _ := degree.Search(".//span[@itemprop='name']")
+    degreevals["school"] = checkVal(school)
 
+    degree_title, _ := degree.Search(".//p[@class='edu_title']")
+    degreevals["degree_title"] = checkVal(degree_title)
+
+    school_location, _ := degree.Search(".//span[@itemprop='addressLocality']")
+    degreevals["school_location"] = checkVal(school_location)
+
+    degree_dates, _ := degree.Search(".//p[@class='edu_dates']")
+    degreevals["start_date"], degreevals["end_date"] = parseDates(checkVal(degree_dates))
+
+    // Add vals that are the same everywhere for person
+    for key, val := range(personvals) {
+      degreevals[key] = val
+    }
+    overall = append(overall, degreevals)
+  }
+  
   // Military service info
+}
+
+// Handles date parsing
+func parseDates(dates string) (string, string){
+  split := strings.Split(dates, " ")
+  start_date := ""
+  end_date := ""
+  
+  if len(split) == 4 {
+    start_date = split[0]
+    end_date = split[3]
+  } else {
+    start_date = dates
+    end_date = dates
+  }
+  return start_date, end_date
 }
 
 // Gets the body of a webpage
