f6c6016a73ee695d9a4e1b07609da7cd99bfbcf1
GH <shidash@shidash.com>
2015-04-01 18:02:30 +0200

    Changed to go version

---
+++ indeedscraper.go
@@ -0,0 +1,110 @@
+package indeedscraper
+
+import (
+  "net/http"
+  "crypto/tls"
+  "io/ioutil"
+	"strings"
+	"strconv"
+	"encoding/json"
+  "github.com/moovweb/gokogiri"
+)
+
+//TODO:
+// Interface for certifications, awards, education, military, groups, links as two-level json
+// Script to run (like li one)
+
+// Add concurrency (in parsing and pages?)
+// Add same for job listing
+
+var overall []map[string]string
+
+// Download all the resumes
+func GetResumes(searchterm string, location string) string {
+  // Generate search URL
+  searchterm = cleanString("golang")
+  location = cleanString("")
+  url := "http://indeed.com/resumes?"
+
+  // Add search term to URL
+  if searchterm != "" {
+    url += "q="+searchterm
+  }
+
+  // Add location to URL
+  if location != "" {
+    if strings.Contains(url, "?q="){
+      url += "&"
+    }
+    url += "l="+location
+  }
+
+
+  // Get page with results
+  body := getPage(url)
+  numPages := getPageCount(body)
+
+  // Loop through all pages
+  for i := 0; i < numPages; i++ {
+    getResults(url + "&start="+strconv.Itoa(i*50))
+  }
+
+  out, _ := json.MarshalIndent(overall, "", "    ")
+  return string(out)
+}
+
+// Gets the results for a single page
+func getResults(resultsurl string) {
+  body := getPage(resultsurl)
+  
+	// Get a list of all profile links on page
+	doc, _ := gokogiri.ParseHtml(body)
+	results, _ := doc.NodeById("results").Search("//li[@itemtype='http://schema.org/Person']")
+	names, _ := results[0].Search("//a[@class='app_link']")
+
+	// Send link of each profile on page to parser
+	for _, profile := range(names){
+    parseProfile("http://indeed.com"+profile.Attr("href"))
+	}
+}
+
+// Gets the total number of result pages
+func getPageCount(firstpage []uint8) int {
+	parsed, _ := gokogiri.ParseHtml(firstpage)
+	numresults, _ := parsed.Search("//div[@id='result_count']")
+	num, _ := strconv.Atoi(strings.Split(numresults[0].InnerHtml(), " ")[1])
+  
+	numpages := num/50
+	if num % 50 != 0 {
+	  numpages += 1
+	}
+
+	return numpages
+}
+
+// Gets the body of a webpage
+func getPage(url string) []uint8 {
+  // SSL config
+  tlsConfig := &tls.Config{
+    InsecureSkipVerify: true,
+  }
+  transport := &http.Transport{
+    TLSClientConfig: tlsConfig,
+  }
+  client := http.Client{Transport: transport}
+
+  // Get page for search term
+  resp, _ := client.Get(url)
+  defer resp.Body.Close()
+  body, _ := ioutil.ReadAll(resp.Body)
+  
+  return body
+}
+
+// Format search string as needed for URL params
+func cleanString(input_term string) string {
+  outstr := strings.Replace(input_term, " ", "+", -1)
+  outstr = strings.Replace(outstr, ",", "%2C", -1)
+  
+  return outstr
+}
--- parse_resume.go
+++ parse_resume.go
@@ -1,125 +1,40 @@
-package main
+package indeedscraper
 
-import (
-  "fmt"
-  "net/http"
-  "crypto/tls"
-  "io/ioutil"
-	"strings"
-	"strconv"
+import(
   "time"
-	"encoding/json"
+  "encoding/json"
   "github.com/moovweb/gokogiri"
+  "strings"
   "github.com/moovweb/gokogiri/xml"
 )
-
-//TODO: 
-// Call externally (input opts, return vals), mult files, package
-// Add concurrency (in parsing and pages?)
-// Interface for certifications, awards, education, military, groups, links as two-level json
-
-// Add same for job listing
-
-var overall []map[string]string
-
-func main(){
-  // Generate search URL
-  searchterm := cleanString("golang")
-  location := cleanString("")
-  url := "http://indeed.com/resumes?"
-
-  // Add search term to URL
-  if searchterm != "" {
-    url += "q="+searchterm
-  }
-
-  // Add location to URL
-  if location != "" {
-    if strings.Contains(url, "?q="){
-      url += "&"
-    }
-    url += "l="+location
-  }
-  
-  
-  // Get page with results
-	body := getPage(url)
-	numPages := getPageCount(body)
-
-  // Loop through all pages
-  for i := 0; i < numPages; i++ {
-    getResults(url + "&start="+strconv.Itoa(i*50))
-  }
-  
-  out, _ := json.MarshalIndent(overall, "", "    ")
-  fmt.Println(string(out))
-}
-
-// Gets the results for a single page
-func getResults(resultsurl string) {
-  body := getPage(resultsurl)
-  
-	// Get a list of all profile links on page
-	doc, _ := gokogiri.ParseHtml(body)
-	results, _ := doc.NodeById("results").Search("//li[@itemtype='http://schema.org/Person']")
-	names, _ := results[0].Search("//a[@class='app_link']")
-
-	// Send link of each profile on page to parser
-	for _, profile := range(names){
-    parseProfile("http://indeed.com"+profile.Attr("href"))
-	}
-}
-
-// Gets the total number of result pages
-func getPageCount(firstpage []uint8) int {
-	parsed, _ := gokogiri.ParseHtml(firstpage)
-	numresults, _ := parsed.Search("//div[@id='result_count']")
-	num, _ := strconv.Atoi(strings.Split(numresults[0].InnerHtml(), " ")[1])
-  
-	numpages := num/50
-	if num % 50 != 0 {
-	  numpages += 1
-	}
-
-	return numpages
-}
-
-// Return cleaned up value if input isn't empty
-func checkVal(input []xml.Node) string {
-  if(len(input) != 0){
-    return input[0].InnerHtml()
-  } else {
-    return ""
-  }
-}
 
 // Parses and saves data in profile
 func parseProfile(url string) {
   body := getPage(url)
   doc, _ := gokogiri.ParseHtml(body)
-  
+
   // Set the values that are the same for all items in profile
   personvals := make(map[string]string)
-	
+
   name, _ := doc.Search("//h1[@itemprop='name']")
   personvals["name"] = checkVal(name)
-	
+
   personvals["url"] = url
   personvals["time_scraped"] = time.Now().Format(time.RFC850)
   personvals["fulltext"] = string(body)
-	
+
   location, _ := doc.Search("//p[@id='headline_location']")
   personvals["location"] = checkVal(location)
-	
+
   current_title, _ := doc.Search("//h2[@id='headline']")
   personvals["current_title"] = checkVal(current_title)
-	
+
   skills, _ := doc.Search("//div[@id='skills-section']//p")
   personvals["skills"] = checkVal(skills)
 
   summary, _ := doc.Search("//p[@id='res_summary']")
   personvals["summary"] = checkVal(summary)
-	
+
   additional_info, _ := doc.Search("//div[@id='additionalinfo-section']//p")
   personvals["additional_info"] = checkVal(additional_info)
 
@@ -127,13 +42,13 @@ func parseProfile(url string) {
   linkslice := make([]map[string]string, len(links))
   for n, link := range(links){
     linkvals := make(map[string]string)
-    
+
     link_title, _ := link.Search(".//a")
     linkvals["link_title"] = checkVal(link_title)
 
     link_url, _ := link.Search(".//a")
     linkvals["link_url"] = link_url[0].Attr("href")
-    
+
     linkslice[n] = linkvals
   }
   linklist, _ := json.Marshal(linkslice)
@@ -153,7 +68,7 @@ func parseProfile(url string) {
 
     award_description, _ := award.Search(".//p[@class='award_description']")
     awardvals["award_description"] = checkVal(award_description)
-    
+
     awardslice[n] = awardvals
   }
   awardlist, _ := json.Marshal(awardslice)
@@ -252,10 +167,10 @@ func parseProfile(url string) {
   personvals["degrees"] = string(degreelist)
 
   // Work history
-	jobs, _ := doc.Search("//div[contains(concat(' ',normalize-space(@class),' '),' work-experience-section ')]")
-	for _, job := range(jobs) {
+  jobs, _ := doc.Search("//div[contains(concat(' ',normalize-space(@class),' '),' work-experience-section ')]")
+  for _, job := range(jobs) {
     jobvals := make(map[string]string)
-    
+
     job_title, _ := job.Search(".//p[@class='work_title title']")
     jobvals["job_title"] = checkVal(job_title)
 
@@ -270,9 +185,9 @@ func parseProfile(url string) {
 
     job_dates, _ := job.Search(".//p[@class='work_dates']")
     jobvals["start_date"], jobvals["end_date"] = parseDates(checkVal(job_dates))
-    
+
     addPersonVals(personvals, jobvals)
-	}
+  }
 }
 
 // Adds the values that are the same for all items
@@ -280,8 +195,17 @@ func addPersonVals(personvals map[string]string, itemvals map[string]string){
   for key, val := range(personvals) {
     itemvals[key] = val
   }
-  
+
   overall = append(overall, itemvals)
+}
+
+// Return cleaned up value if input isn't empty
+func checkVal(input []xml.Node) string {
+  if(len(input) != 0){
+    return input[0].InnerHtml()
+  } else {
+    return ""
+  }
 }
 
 // Handles date parsing
@@ -304,31 +228,4 @@ func parseDates(dates string) (string, string){
   }
 
   return start_date, end_date
-}
-
-// Gets the body of a webpage
-func getPage(url string) []uint8 {
-  // SSL config
-  tlsConfig := &tls.Config{
-    InsecureSkipVerify: true,
-  }
-  transport := &http.Transport{
-    TLSClientConfig: tlsConfig,
-  }
-  client := http.Client{Transport: transport}
-
-  // Get page for search term
-  resp, _ := client.Get(url)
-  defer resp.Body.Close()
-  body, _ := ioutil.ReadAll(resp.Body)
-  
-  return body
-}
-
-// Format search string as needed for URL params
-func cleanString(input_term string) string {
-  outstr := strings.Replace(input_term, " ", "+", -1)
-  outstr = strings.Replace(outstr, ",", "%2C", -1)
-  
-  return outstr
 }
